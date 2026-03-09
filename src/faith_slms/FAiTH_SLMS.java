e6bc40291c1deb3c2945f4e45c325493543fe91b
Fooza <zaidiisa2@gmail.com>
2026-03-09 22:21:47 +0800

    Completed menu for main class

--- src/faith_slms/FAiTH_SLMS.java
+++ src/faith_slms/FAiTH_SLMS.java
@@ -16,16 +16,19 @@ public class FAiTH_SLMS {
     public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
       int choice = 0;
-      CourseManager manager = new CourseManager(); //COurseManager object will contain Course array ~Fauzan
+      CourseManager manager = new CourseManager(); //CourseManager object will contain Course array ~Fauzan
       
-      do{ //Main loop for the menu. Will be completed later ~Fauzan == already completed by haris
+      do{ // (9/3/2026) Extended Haris version of menu ~Fauzan
           System.out.println("\n===== COURSE MANAGEMENT =====");
           System.out.println("1. Create Course");
           System.out.println("2. Display Courses");
           System.out.println("3. Search Course");
+          System.out.println("4. Edit Course");
+          System.out.println("5. Delete Course");
           System.out.println("-1. Exit");
           System.out.print("Enter choice: ");
           choice = input.nextInt();
+          System.out.print("\n");
           switch(choice){
               case 1:
                   manager.createCourse();
@@ -35,7 +38,13 @@ public class FAiTH_SLMS {
                   break;
               case 3:
                   manager.searchCourse();
-        break;
+                  break;
+              case 4:
+                  manager.editCourse();
+                  break;
+              case 5:
+                  manager.deleteCourse();
+                  break;
           } 
     }while(choice != -1);
       
